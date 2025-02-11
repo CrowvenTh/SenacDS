1274ab72778b58fe0cd059921a59dc560cf6c4d9
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-11 17:27:05 -0300

    resolução de exercicios 11 e 12

---
+++ algoritmo/Atividades/A12_capitalize.py
@@ -0,0 +1,5 @@
+# Escreva um programa em Python que utilize a variável texto= "olá mundo!" e imprima a primeira letra do texto em maiúscula.
+
+palavra = "olá mundo"
+
+print(palavra.capitalize())
--- algoritmo/Atividades/A2_impressaoDeNumeros.py
+++ algoritmo/Atividades/A2_impressaoDeNumeros.py
@@ -1,4 +1,4 @@
-# # Crie um programa que imprima os números de 1 até 10.
+# Crie um programa que imprima os números de 1 até 10.
 
 # numero = [1,2,3,4,5,6,7,8,9,10]
 # print(numero)
