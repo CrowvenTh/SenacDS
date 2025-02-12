0009e9f615bb45a4424871ab7edacee9e5f133ed
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-12 17:07:29 -0300

    resolucao A17

---
+++ algoritmo/Atividades/A17_inputSubtracao.py
@@ -0,0 +1,6 @@
+# Escreva um programa em Python que peça ao usuário para inserir dois números e calcule a subtração do segundo número pelo primeiro. Em seguida, exiba o resultado da subtração.
+n1 = int(input("Insira o primeiro número: "))
+n2 = int(input("Insira o segundo número: "))
+sub = (n1 - n2)
+
+print(f"{n1} - {n2} é igual a {sub}")
