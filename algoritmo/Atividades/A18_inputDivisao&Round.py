1e9fa63693326ce43f2abe2651700d98487d0769
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-12 17:13:15 -0300

    resolucao A18

---
+++ algoritmo/Atividades/A18_inputDivisao&Round.py
@@ -0,0 +1,7 @@
+# Escreva um programa em Python que peça ao usuário para inserir dois números e calcule a divisão do primeiro número pelo segundo número. Certifique-se de verificar se o segundo número não é zero antes de realizar a divisão. Em seguida, exiba o resultado da divisão.
+n1 = int(input("Insira o primeiro número: "))
+n2 = int(input("Insira o segundo número: "))
+
+divArredondada = round(n1 / n2)
+
+print(f"{n1} dividido por {n2} é igual a {divArredondada}")
