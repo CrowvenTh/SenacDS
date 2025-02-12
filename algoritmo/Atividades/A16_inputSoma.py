f27d03e4434641b5d45670e629293de830160ed6
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-12 17:02:31 -0300

    resolucao A16

---
+++ algoritmo/Atividades/A16_inputSoma.py
@@ -0,0 +1,6 @@
+#  Escreva um programa em Python que peça ao usuário para inserir dois números e calcule a soma desses números. Em seguida, exiba o resultado da soma.
+n1 = int(input("Insira o primeiro numero: "))
+n2 = int(input("Insira o segundo numero: "))
+soma = (n1 + n2)
+
+print(f"{n1} + {n2} é igual a {soma}")
