e4ceba8ef1947c97dd40ce81d23ec5177ef19b2f
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-12 16:57:18 -0300

    resolucao A15

---
+++ algoritmo/Atividades/A15_inputNome.py
@@ -0,0 +1,4 @@
+# Escreva um programa em Python que solicite ao usuário para inserir seu nome. O programa deve exibir uma mensagem de boas-vindas personalizada, incluindo o nome inserido pelo usuário.
+
+nome = input("Bem vindo, insira seu nome: ")
+print("O nome inserido foi:",nome)
