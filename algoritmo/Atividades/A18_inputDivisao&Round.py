2396f991f02d0e841076f47b9318c98f332fbc01
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-12 17:19:05 -0300

    resolucao A18 com fround

--- algoritmo/Atividades/A18_inputDivisao&Round.py
+++ algoritmo/Atividades/A18_inputDivisao&Round.py
@@ -2,6 +2,6 @@
 n1 = float(input("Insira o primeiro número: "))
 n2 = float(input("Insira o segundo número: "))
 
-divArredondada = round(n1 / n2)
+divArredondada = round(n1 / n2, 2)
 
 print(f"{n1} dividido por {n2} é igual a {divArredondada}")
