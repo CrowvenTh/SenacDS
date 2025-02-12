5f6ce9ce50358e1a3b32c2fdb28803cfd96fa9f0
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-12 16:50:40 -0300

    resolucao A14

---
+++ algoritmo/Atividades/A14_strip&replace.py
@@ -0,0 +1,8 @@
+# Escreva um programa em Python que realize as seguintes operações em uma frase pré-definida:
+# A frase fornecida é: " O dia está bom, mas o tempo está chuvoso. "
+# Remova quaisquer espaços em branco extras no início e no final da frase.
+# Substitua todas as ocorrências da palavra "bom" por "ótimo".
+# Ao final, o programa deve exibir a frase sem espaços extras e com as substituições realizadas.
+
+frase =  " O dia está bom, mas o tempo está chuvoso. "
+print(frase.strip().replace("bom", "ótimo"))
