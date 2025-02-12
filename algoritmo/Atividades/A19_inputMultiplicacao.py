b2e9441048d0bcf3f65480ff5cd5b70e4c30e97c
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-12 17:25:40 -0300

    resolucao A19

--- algoritmo/Atividades/A19_inputMultiplicacao.py
+++ algoritmo/Atividades/A19_inputMultiplicacao.py
@@ -4,4 +4,4 @@ number2 = int(input("Insira o segundo número: "))
 
 produto = (number1 * number2)
 
-print(f"{number1} X {number2} é igual a {produto}")
+print(f"{number1} x {number2} é igual a {produto}")
