869dd53bca3eb5198b97fdd2cfaf18e166e711f4
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-12 17:24:34 -0300

    resolucao A19

---
+++ algoritmo/Atividades/A19_inputMultiplicacao.py
@@ -0,0 +1,7 @@
+# Escreva um programa em Python que peça ao usuário para inserir dois números reais e calcule o produto desses números. Em seguida, exiba o resultado da multiplicação.
+number1 = int(input("Insira o primeiro número: "))
+number2 = int(input("Insira o segundo número: "))
+
+produto = (number1 * number2)
+
+print(f"{number1} X {number2} é igual a {produto}")
