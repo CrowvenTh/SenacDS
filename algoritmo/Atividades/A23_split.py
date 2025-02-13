d03e590ccc1a7a4441856b70906b731d76bbc7bf
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-13 15:25:50 -0300

    resolucao A23

---
+++ algoritmo/Atividades/A23_split.py
@@ -0,0 +1,5 @@
+# Escreva um programa em Python que aceite uma sequência de linguagens de programação separadas por espaços. O programa deve dividir essa sequência em uma lista de linguagens individuais e imprimir a lista resultante. Ao final imprimir o tipo da variável.
+
+Linguagens = "Python Java C# C++ PHP"
+l = Linguagens.split(" ")
+print(l)
