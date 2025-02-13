92e334488409fa23aa5750b444c444ce05bd0f3a
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-13 16:27:03 -0300

    resolucao A26

---
+++ algoritmo/Atividades/A26_tuplaIndice.py
@@ -0,0 +1,5 @@
+# Crie um programa que receba a tupla abaixo e imprima a linguem de programação: C++
+# tupla: "Python","Java","C#","C++","PHP"
+
+tupla = "Python","Java","C#","C++","PHP"
+print(tupla[3])
