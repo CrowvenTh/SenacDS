42bdbcdbd594a41b7f1b4c896b587f8d60ff64d7
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-13 16:21:32 -0300

    resolucao a25

---
+++ algoritmo/Atividades/A25_listaIndice.py
@@ -0,0 +1,5 @@
+# Crie um programa que receba a lista abaixo e imprima a linguaguem de programação:
+# lista: ["Python","Java","C#","C++","PHP"]
+
+lista = ["Python","Java","C#","C++","PHP"]
+print(lista[1])
