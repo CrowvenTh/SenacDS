9c5bf06e36e02150be6504d0bc532e2ba774f2c7
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-14 15:27:30 -0300

    resolução A30

---
+++ algoritmo/Atividades/A30_f-string.py
@@ -0,0 +1,3 @@
+# Peça ao usuário para inserir seu nome. Em seguida, use uma f-string para exibir uma mensagem de saudação personalizada.
+nome = input("Insira seu nome: ")
+print(f"Olá {nome}, seja bem vindo!")
