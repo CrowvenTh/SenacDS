3f95e5975ab8fc5cbe5258dc4ab7b50b1cf29c4e
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-14 16:16:40 -0300

    resolução A30

---
+++ algoritmo/Atividades/A31_f-stringPessoa.py
@@ -0,0 +1,7 @@
+# Peça ao usuário para inserir seu nome, idade e cidade. Em seguida, use uma f-string para exibir essas informações formatadas.
+
+nome =  input("insira seu nome: ")
+idade = input("insira sua idade: ")
+Cidade =  input("insira sua cidade: ")
+
+print(f"Seu nome é {nome}, sua  idade é {idade} e você mora em {Cidade}")
