02db509c146273888eca552944ec9e048d3b265f
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-17 14:46:55 -0300

    resolucao A35

---
+++ algoritmo/Atividades/A35_parImpar.py
@@ -0,0 +1,8 @@
+# Crie um programa que determine se um número é par ou ímpar.
+
+number = int(input("insira um número: "))
+
+if(number % 2 == 0):
+    print("O número",number,"é par!")
+else: 
+    print("O número",number,"é impar!")
