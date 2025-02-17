1d4bcc8b4a4e6f38aaea90d00d547adf72dcce42
Thiago Silva <129080167+CrowvenTh@users.noreply.github.com>
2025-02-17 17:16:58 +0000

    resolução A34

---
+++ algoritmo/Atividades/A34_maiorIdade.py
@@ -0,0 +1,7 @@
+# Crie um programa que verifique se uma pessoa pode votar com base em sua idade (idade >= 16).
+idade = int(input("Insira sua idade: "))
+
+if(idade >= 18):
+    print("Você é maior de idade!")
+else:
+    print("Você é menor de idade!")
