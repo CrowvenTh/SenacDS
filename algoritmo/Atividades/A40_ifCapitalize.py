44694698adf3d01af1b55b74543ee91bf65c9f2c
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-17 17:00:32 -0300

    resolução A40

---
+++ algoritmo/Atividades/A40_ifCapitalize.py
@@ -0,0 +1,6 @@
+# Crie um programa que verifique se a primeira letra é maiúscula, caso não seja, capitalize a primeira letra de uma palavra.
+
+palavra = input("Digite uma palavra: ")
+
+if palavra != palavra.capitalize():
+    print(palavra.capitalize())
