058987df287d2332c878ce5cfb6f4b4f90ea6385
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-17 15:03:47 -0300

    resolução A37

---
+++ algoritmo/Atividades/A37_count.py
@@ -0,0 +1,5 @@
+# Faça um programa que transforme um texto todo em letras maiúsculas e conte quantas letras 'A' ele possui.
+
+palavra = input("Insira uma palavra: ")
+
+print(palavra.upper().count("A"))
