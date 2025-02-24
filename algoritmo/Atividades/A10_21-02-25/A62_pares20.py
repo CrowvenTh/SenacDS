11bc081376b46ddef1b3781af5a490543e67d73a
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-24 14:41:48 -0300

    add alternative answer A62

--- algoritmo/Atividades/A10_21-02-25/A62_pares20.py
+++ algoritmo/Atividades/A10_21-02-25/A62_pares20.py
@@ -3,3 +3,11 @@
 for i in range(1, 21):
     if(i % 2 == 0):
         print(i)
+
+# ou da segunda forma para mostrar em apenas uma linha no terminal
+
+concaten = ""
+for i in range(1, 21):
+    if(i % 2 == 0):
+        concaten += str(i) + ", "
+print(concaten)        
