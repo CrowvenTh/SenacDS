f1ea8f2194b8080e769c1304cb6d1b5c1978000e
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-25 17:23:01 -0300

    A74 update

--- README.md
+++ README.md
@@ -2243,6 +2243,22 @@ def maiorNumero():
     return resultado
     
 print(maiorNumero())
+
+# OU
+
+n = int(input("Insira um número: "))
+n1 = int(input("Insira um número: "))
+n2 = int(input("Insira um número: "))
+
+def maiorNumero():
+    if(n > n1 and n > n2):
+        print(f"{n} é maior que {n1} e {n2}")
+    elif(n1 > n2 and n1 > n):
+        print(f"{n1} é maior que {n2} e {n}")
+    elif(n2 > n and n2 > n1):
+        print(f"{n2} é maior que {n} e {n1}")
+
+maiorNumero()
 ~~~~
 
 #### resultado:
--- algoritmo/Atividades/A12_25-02-25/A74_maiorNumero.py
+++ algoritmo/Atividades/A12_25-02-25/A74_maiorNumero.py
@@ -6,11 +6,10 @@ n2 = int(input("Insira um número: "))
 
 def maiorNumero():
     if(n > n1 and n > n2):
-        resultado = f"{n} é maior que {n1} e {n2}"
+        print(f"{n} é maior que {n1} e {n2}")
     elif(n1 > n2 and n1 > n):
-        resultado = f"{n1} é maior que {n2} e {n}"
+        print(f"{n1} é maior que {n2} e {n}")
     elif(n2 > n and n2 > n1):
-        resultado = f"{n2} é maior que {n} e {n1}"
-    return resultado
-    
-print(maiorNumero())
+        print(f"{n2} é maior que {n} e {n1}")
+
+maiorNumero()
