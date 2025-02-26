bf68fd22e30e6f159e9b2ef4cc0335ccbf80a1cd
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-26 14:31:50 -0300

    resolução A76 aula 12

---
+++ algoritmo/Atividades/A12_25-02-25/A76_defMedia.py
@@ -0,0 +1,20 @@
+# Crie uma função que receba 4 números e retorne a média dos números
+
+n1 = float(input("Insira um número: "))
+n2 = float(input("Insira um número: "))
+n3 = float(input("Insira um número: "))
+n4 = float(input("Insira um número: "))
+
+def Media():
+    media = (n1 + n2 + n3 + n4) / 4
+    print(f"A média dos números é {media:.2f}")
+
+Media()
+
+# OU
+
+def Media(n1,n2,n3,n4):
+    media = (n1 + n2 + n3 + n4) / 4
+    return media
+
+print(Media(n1,n2,n3,n4))
