3531e11784b5837e2ec7592df74ae6c715efb57a
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-03-20 17:00:32 -0300

    add foreign key

--- algoritmo/Atividades/BD/A25_20-02-25/script/database.sql
+++ algoritmo/Atividades/BD/A25_20-02-25/script/database.sql
@@ -2,6 +2,13 @@ create schema turma;
 use turma;
 
 create table aluno(
-id_aluno int auto_increment primary key,
-nome varchar(150) not null
+	id_aluno int auto_increment primary key,
+	nome varchar(150) not null
+);
+
+create table trabalho(
+	id_trabalho int auto_increment primary key,
+	descricao varchar(255) not null,
+	id_aluno int not null,
+		foreign key (id_aluno) references aluno(id_aluno)
 );
