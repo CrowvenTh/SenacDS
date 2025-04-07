5b2841394079522522bd5dc4d9b2311bcd603bc3
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-04-07 17:01:39 -0300

    complete projeto final parte 1

--- algoritmo/Atividades/BD/A32_03-04-25/projetofinalDB.sql
+++ algoritmo/Atividades/BD/A32_03-04-25/projetofinalDB.sql
@@ -1,7 +1,6 @@
+drop schema projetofinalDB;
 create schema projetofinalDB;
 use projetofinalDB;
-
-drop table clientes;
 
 create table clientes(
 	cliente_id int auto_increment,
@@ -40,7 +39,7 @@ create table contas(
 	cliente_id int,
 	agencia_id int,
 	tipo_conta enum('Corrente', 'Poupança', 'Salário'),
-	saldo decimal(10,2) default 0.00,
+	saldo decimal(15,2) default 0.00,
 	data_abertura date,
 	status enum('Ativa', 'Inativa', 'Bloqueada') default 'Ativa',
 		primary key(conta_id),
@@ -49,8 +48,9 @@ create table contas(
 );
 
 INSERT INTO Contas (cliente_id, agencia_id, tipo_conta, saldo, data_abertura, status)
-VALUES
+values
 (1, 1, 'Corrente', 5000.00, '2020-01-10', 'Ativa');
+
 
 select * from contas;
 
@@ -59,10 +59,46 @@ create table transacoes(
 	conta_origem_id int,
 	conta_destino_id int,
 	tipo_transacao enum('Depósito', 'Saque', 'Transferencia','Pagamento'),
-	valor decimal(10,2),
+	valor decimal(15,2),
 	data_transacao datetime default CURRENT_TIMESTAMP(),
 	descricao varchar(255),
 		primary key(transacao_id),
 		foreign key(conta_origem_id) references contas(conta_id),
 		foreign key(conta_destino_id) references contas(conta_id)
 );
+
+INSERT INTO Transacoes (conta_origem_id, conta_destino_id, tipo_transacao, valor,
+descricao) VALUES
+(1, NULL, 'Depósito', 1000.00, 'Depósito inicial'),
+(NULL, 2, 'Depósito', 2000.00, 'Depósito inicial');
+
+create table emprestimos(
+	emprestimo_id int auto_increment,
+	conta_id int,
+	valor decimal(15,2),
+	taxa_juros decimal(5,2),
+	parcelas int,
+	valor_parcela decimal(15,2),
+	data_contratacao date,
+	status enum('Ativo','Quitado','Inadiplente') default 'Ativo',
+		primary key(emprestimo_id),
+		foreign key(conta_id) references contas(conta_id)
+);
+
+INSERT INTO Emprestimos (conta_id, valor, taxa_juros, parcelas, valor_parcela,
+data_contratacao) VALUES
+(1, 10000.00, 1.5, 12, 916.67, '2022-01-15');
+
+create table pagamentosEmprestimos(
+	pagamento_id int auto_increment,
+	emprestimo_id int,
+	numero_parcela int,
+	valor_pago decimal(15,2),
+	data_pagamento date,
+		primary key(pagamento_id),
+		foreign key(emprestimo_id) references emprestimos(emprestimo_id)
+);
+
+INSERT INTO PagamentosEmprestimos (emprestimo_id, numero_parcela, valor_pago,
+data_pagamento) VALUES
+(1, 1, 916.67, '2022-02-15');
